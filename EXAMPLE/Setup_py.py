766432520095d70ecac6d38c3408b109f9594a0c
Kalderon <Cem.Kalderon@ing.com>
2019-11-13 14:15:16 +0100

    Commit_CK_14.15hrs

--- EXAMPLE/Setup_py.py
+++ EXAMPLE/Setup_py.py
@@ -14,13 +14,13 @@ extra_requirements = {
 
 
 setuptools.setup(
-    name='pyjpeg',
+    name='ds-bootcamp-test',
     author='Cem Kalderon',
     author_email='cemkalderon@gmail.com',
     description='JPEG implementation in Python.',
     url='https://github.com/JCZuurmond/pyjpeg',
     license='Open source',
-    packages=setuptools.find_packages('pyjpeg'),
+    packages=setuptools.find_packages('.'),
     version='0.1.0',
     install_requires=requirements,
     setup_requires=setup_requirements,
