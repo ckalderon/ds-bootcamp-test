91041cccf709a0e09edf54b772478a538c603f2d
Kalderon <Cem.Kalderon@ing.com>
2019-11-13 12:34:59 +0100

    commit_ck_12_34hrs

---
+++ EXAMPLE/Setup_py.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
+requirements = []
+setup_requirements = []
+test_requirements = ['pytest']
+extra_requirements = {
+    'dev': ['pre-commit', 'flake8']
+}
+
+
+setuptools.setup(
+    name='pyjpeg',
+    author='Cem Kalderon',
+    author_email='cemkalderon@gmail.com',
+    description='JPEG implementation in Python.',
+    url='https://github.com/JCZuurmond/pyjpeg',
+    license='Open source',
+    packages=setuptools.find_packages('pyjpeg'),
+    version='0.1.0',
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    extras_require=extra_requirements,
+)
